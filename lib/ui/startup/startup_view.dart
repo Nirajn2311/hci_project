f709106c6f28c59c738e766b06cb8c991339658a
Niraj Nandish <nirajnandish@icloud.com>
2022-02-07 14:26:59 +0400

    basic qr scanner

--- lib/ui/startup/startup_view.dart
+++ lib/ui/startup/startup_view.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hci_project/ui/startup/startup_viewmodel.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:stacked/stacked.dart';
 
 class StartUpView extends StatelessWidget {
@@ -7,16 +11,62 @@ class StartUpView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double? scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+
+    void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+      log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+      if (!p) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('no Permission')),
+        );
+      }
+    }
+
     return ViewModelBuilder<StartUpViewModel>.reactive(
+      viewModelBuilder: () => StartUpViewModel(),
       builder: (context, model, child) => Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: model.doSomething,
+        appBar: AppBar(
+          title: const Text('HCI Project'),
         ),
-        body: Center(
-          child: Text(model.title),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 4,
+              child: QRView(
+                key: model.qrKey,
+                onQRViewCreated: model.onQRViewCreated,
+                overlay: QrScannerOverlayShape(
+                  borderColor: Colors.red,
+                  borderRadius: 10,
+                  borderLength: 30,
+                  borderWidth: 10,
+                  cutOutSize: scanArea,
+                ),
+                onPermissionSet: (ctrl, p) =>
+                    _onPermissionSet(context, ctrl, p),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    model.result != null
+                        ? Text(
+                            'Type: ${describeEnum(model.result!.format)}   Data: ${model.result!.code}')
+                        : const Text('Scan a code')
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-      viewModelBuilder: () => StartUpViewModel(),
     );
   }
 }
--- lib/ui/startup/startup_viewmodel.dart
+++ lib/ui/startup/startup_viewmodel.dart
@@ -1,12 +1,39 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class StartUpViewModel extends BaseViewModel {
   String title = '';
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
 
   void doSomething() {
     title += 'updated ';
     // this will call the builder defined in the view file and rebuild the ui using
     // the update version of the model.
     notifyListeners();
+  }
+
+  void openCamera() {
+    log('Camera opened');
+  }
+
+  void onQRViewCreated(QRViewController qrController) {
+    controller = qrController;
+    notifyListeners();
+    controller?.scannedDataStream.listen((scanData) {
+      result = scanData;
+      log(result.toString());
+      notifyListeners();
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
