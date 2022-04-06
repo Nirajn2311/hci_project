4462efbc2b826def1fb027b3ab38669e44beba11
Niraj Nandish <nirajnandish@icloud.com>
2022-04-06 23:07:26 +0530

    add graph

--- lib/ui/startup/startup_view.dart
+++ lib/ui/startup/startup_view.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hci_project/ui/startup/startup_viewmodel.dart';
+import 'package:intl/intl.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:stacked/stacked.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class StartUpView extends StatelessWidget {
   const StartUpView({Key? key}) : super(key: key);
@@ -26,25 +27,46 @@ class StartUpView extends StatelessWidget {
             Expanded(
               flex: 1,
               child: SingleChildScrollView(
-                child: model.result != null && model.fieldValues != {}
-                    ? Column(
-                        children: [
-                          Text(
-                            '${model.result?['channel']['name']}',
-                            style: const TextStyle(fontSize: 25),
-                          ),
-                          ...model.fieldValues.entries.map(
-                            (entry) => Text(
-                              '${entry.key}: ${entry.value}',
-                              style: const TextStyle(fontSize: 20),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
+                  child: model.result != null && model.sensorValues != {}
+                      ? Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Text(
+                              '${model.result?['channel']['name']}',
+                              style: const TextStyle(fontSize: 25),
+                              textAlign: TextAlign.center,
                             ),
-                          ),
-                        ],
-                      )
-                    : const Text(
-                        'Scan a code',
-                        style: TextStyle(fontSize: 40),
-                      ),
+                            ...model.sensorValues.entries.map(
+                              (entry) => Text(
+                                '${entry.key}: ${entry.value[0].y}',
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                            ),
+                            SfCartesianChart(
+                              primaryXAxis: DateTimeAxis(
+                                edgeLabelPlacement: EdgeLabelPlacement.shift,
+                                intervalType: DateTimeIntervalType.auto,
+                                dateFormat: DateFormat.Hms(),
+                                name: 'Time',
+                                majorGridLines: const MajorGridLines(width: 0),
+                              ),
+                              series: <LineSeries<SensorData, DateTime>>[
+                                LineSeries<SensorData, DateTime>(
+                                  dataSource: model.sensorValues.values.first,
+                                  xValueMapper: (SensorData data, _) => data.x,
+                                  yValueMapper: (SensorData data, _) => data.y,
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      : const Text(
+                          'Scan a code',
+                          style: TextStyle(fontSize: 40),
+                        ),
+                ),
               ),
             ),
           ],
--- lib/ui/startup/startup_viewmodel.dart
+++ lib/ui/startup/startup_viewmodel.dart
@@ -1,32 +1,52 @@
-import 'dart:convert';
 import 'dart:developer';
 
-import 'package:stacked/stacked.dart';
-import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:dio/dio.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:stacked/stacked.dart';
 
 class StartUpViewModel extends BaseViewModel {
   Map<String, dynamic>? result;
   String currQR = '';
-  Map<String, String> fieldValues = {};
+  Map<String, List<SensorData>> sensorValues = {};
+  Map<String, String> fields = {};
   Dio dio = Dio();
 
   void updateQR(Barcode barcode) async {
     log('NEW QR DETECTED');
     currQR = barcode.rawValue!;
     Response dioRes = await dio
-        .get('https://api.thingspeak.com/channels/$currQR/feed.json?results=3');
+        .get('https://api.thingspeak.com/channels/$currQR/feed.json?results=50');
     result = dioRes.data;
-    fieldValues = {};
+    sensorValues = {};
     result?['channel'].entries.forEach((entry) {
       if (entry.key.toString().startsWith('field')) {
-        fieldValues[entry.value] = result?['feeds'][0][entry.key];
+        sensorValues[entry.value] = [];
+        fields[entry.key] = entry.value;
+        // fieldValues[entry.value]?.add([ ,result?['feeds'][0][entry.key]]);
       }
     });
-    fieldValues.forEach((key, value) {
+    result?['feeds'].forEach((feedEntry) {
+      feedEntry.entries.forEach((entry) {
+        if (entry.key.toString().startsWith('field')) {
+          sensorValues[fields[entry.key]]?.add(
+            SensorData(
+              x: DateTime.parse(feedEntry['created_at']),
+              y: double.parse(feedEntry[entry.key]),
+            ),
+          );
+        }
+      });
+    });
+    sensorValues.forEach((key, value) {
       log(key);
-      log(value);
+      log(value[0].toString());
     });
     notifyListeners();
   }
+}
+
+class SensorData {
+  SensorData({this.x, this.y});
+  DateTime? x;
+  num? y;
 }
