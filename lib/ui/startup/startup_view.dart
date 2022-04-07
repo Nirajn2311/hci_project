415b2e4aafdb47db4a76796443740109e56ced3e
Niraj Nandish <nirajnandish@icloud.com>
2022-04-07 14:16:36 +0530

    add bottom slidable sheet

--- lib/ui/startup/startup_view.dart
+++ lib/ui/startup/startup_view.dart
@@ -1,9 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hci_project/ui/startup/startup_viewmodel.dart';
 import 'package:intl/intl.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:stacked/stacked.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class StartUpView extends StatelessWidget {
   const StartUpView({Key? key}) : super(key: key);
@@ -16,60 +19,78 @@ class StartUpView extends StatelessWidget {
         appBar: AppBar(
           title: const Text('HCI Project'),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: MobileScanner(
-                onDetect: (barcode, args) => model.updateQR(barcode),
-              ),
+        body: SlidingUpPanel(
+          renderPanelSheet: false,
+          backdropOpacity: 0.3,
+          panel: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24.0),
+                  topRight: Radius.circular(24.0)),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 20.0,
+                  color: Colors.grey,
+                ),
+              ],
             ),
-            Expanded(
-              flex: 1,
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
-                  child: model.result != null && model.sensorValues != {}
-                      ? Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Text(
-                              '${model.result?['channel']['name']}',
-                              style: const TextStyle(fontSize: 25),
-                              textAlign: TextAlign.center,
-                            ),
-                            ...model.sensorValues.entries.map(
-                              (entry) => Text(
-                                '${entry.key}: ${entry.value[0].y}',
-                                style: const TextStyle(fontSize: 20),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+              child: model.result != null && model.sensorValues != {}
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          '${model.result?['channel']['name']}',
+                          style: const TextStyle(fontSize: 25),
+                          textAlign: TextAlign.center,
+                        ),
+                        ...model.sensorValues.entries.map(
+                          (entry) => Text(
+                            '${entry.key}: ${entry.value[0].y}',
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SfCartesianChart(
+                          primaryXAxis: DateTimeAxis(
+                            edgeLabelPlacement: EdgeLabelPlacement.shift,
+                            intervalType: DateTimeIntervalType.auto,
+                            // dateFormat: DateFormat.Hms(),
+                            name: 'Time',
+                            majorGridLines: const MajorGridLines(width: 0),
+                          ),
+                          series: <LineSeries<SensorData, DateTime>>[
+                            LineSeries<SensorData, DateTime>(
+                              dataSource: model.sensorValues.values.elementAt(
+                                Random().nextInt(
+                                  model.sensorValues.values.length,
+                                ),
                               ),
-                            ),
-                            SfCartesianChart(
-                              primaryXAxis: DateTimeAxis(
-                                edgeLabelPlacement: EdgeLabelPlacement.shift,
-                                intervalType: DateTimeIntervalType.auto,
-                                dateFormat: DateFormat.Hms(),
-                                name: 'Time',
-                                majorGridLines: const MajorGridLines(width: 0),
-                              ),
-                              series: <LineSeries<SensorData, DateTime>>[
-                                LineSeries<SensorData, DateTime>(
-                                  dataSource: model.sensorValues.values.first,
-                                  xValueMapper: (SensorData data, _) => data.x,
-                                  yValueMapper: (SensorData data, _) => data.y,
-                                )
-                              ],
+                              xValueMapper: (SensorData data, _) => data.x,
+                              yValueMapper: (SensorData data, _) => data.y,
                             )
                           ],
                         )
-                      : const Text(
-                          'Scan a code',
-                          style: TextStyle(fontSize: 40),
-                        ),
+                      ],
+                    )
+                  : const Text(
+                      'Scan a code',
+                      style: TextStyle(fontSize: 40),
+                      textAlign: TextAlign.center,
+                    ),
+            ),
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                // flex: 2,
+                child: MobileScanner(
+                  onDetect: (barcode, args) => model.updateQR(barcode),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
